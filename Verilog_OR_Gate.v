e6f663d27f624941a039963b31e746d7fe20b35c
Akshad Dhamande <162341320+Akshad2911@users.noreply.github.com>
2026-06-30 23:24:56 +0530

    Create AND gate module in Verilog

---
+++ Verilog_OR_Gate.v
@@ -0,0 +1,13 @@
+//==================================================
+// AND Gate - Verilog HDL
+//==================================================
+
+module and_gate (
+    input  A,
+    input  B,
+    output Y
+);
+
+    assign Y = A & B;
+
+endmodule
